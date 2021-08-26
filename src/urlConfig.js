0783573a4f238f70fa3ea921b8f33beb67cf3ce4
igopalakrishnan <kingkrishna809@gmail.com>
2021-08-26 17:41:05 +0530

    update url config

--- src/urlConfig.js
+++ src/urlConfig.js
@@ -1,15 +1,15 @@
-/* export const baseUrl = "https://ecommerce-back-end-rest-server.herokuapp.com"
+export const baseUrl = "https://ecommerce-back-end-rest-server.herokuapp.com"
 
 export const api = `${baseUrl}/api`;
 export const generatePublicUrl = (fileName) => {
     return `${baseUrl}/public/${fileName}`;
-} */
+}
 
 
 
-export const api = 'http://localhost:2000/api';
+/* export const api = 'http://localhost:2000/api';
 
 export const generatePublicUrl = (fileName) => {
     return `http://localhost:2000/public/${fileName}`;
 }
-
+ */
